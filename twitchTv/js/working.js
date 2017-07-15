0f193885f8190d8d11570dd6b582a6645488ce8e
Emmanuel Ndukwe <emasysnd@gmail.com>
2017-07-15 21:57:53 +0100

    twitch tv v1.2

--- twitchTv/js/working.js
+++ twitchTv/js/working.js
@@ -11,36 +11,34 @@ var channels = ["DreadzTV","EULCS1",
     "StarCraft", "Ninja", "ESL_SC2"];
 //Variable declaration
 var url,channelName,channelUrl, channelLogo, content;
+
 function getChannelInfo() {
     channels.forEach(function(channel) {
         function makeURL(type, name) {
             return 'https://wind-bow.gomix.me/twitch-api/' + type + '/' + name + '?callback=?';
         };
         $.getJSON(makeURL("streams", channel), function(data) {
+//                var game,
+//                    status;
             var showing, streaming;
             if (data.stream === null) {
-                showing = "Nothing";
-                streaming = '<span class="offline">offline</span>';
+                showing = "Offline";
+                streaming = "offline";
             } else if (data.stream === undefined) {
                 showing = "Account Closed";
-                streaming = '<span class="offline">offline</span>';
+                streaming = "offline";
             } else {
                 showing = data.stream.game;
-                streaming = '<span class="online">online</span>';
+                streaming = "online";
             };
             $.getJSON(makeURL("channels", channel), function(data) {
                 channelLogo = data.logo;
                 channelUrl = data.url;
                 channelName = data.display_name;
-                content = "<div class=\"card\" >" +
+                content = "<div class=\"card\">" +
                     "<div class=\"row\">" +
                     "<div class=\"col-3 \"><img src='" + channelLogo + "' class='iconize'></div>" +
-                    "<div class=\"col-4  hidden-xs-down text-right cardText\">" +
-                    "<h5 class='title'>Channel Name:</h5>" +
-                    "<p class=\"card-lines\">Currently Streaming:</p>" +
-                    "<p class=\"card-lines borders status\" >Status:</p>" +
-                    "</div>"+
-                    "<div class=\"col-8  col-sm-5 offset-1 offset-sm-0 cardText\">" +
+                    "<div class=\"col-9  cardText\">" +
                     "<h5 class='title'><a href='" + channelUrl + "' target='_blank'>" + channelName + "</a></h5>" +
                     "<p class=\"card-lines\">" + showing + "</p>" +
                     "<p class=\"card-lines borders status\" >" + streaming + "</p>" +
@@ -53,9 +51,4 @@ function getChannelInfo() {
 
 $(document).ready(function(){
     getChannelInfo();
-    $(".nav-link").click(function() {
-        $(".nav-link").removeClass("active");
-        $(this).addClass("active");
-
-    });
 });
