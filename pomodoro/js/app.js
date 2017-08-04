6d69c9cf597225842719c556a07121b06a1b2bc0
Emmanuel Ndukwe <etechgist@gmail.com>
2017-08-04 18:51:50 +0100

    added pomodoro clock

---
+++ pomodoro/js/app.js
@@ -0,0 +1,113 @@
+/**
+ * Created by eMASYS ND on 8/3/2017.
+ * credits: https://mrkaluzny.com/
+ */
+
+var sessionLength = 25;
+var breakLength = 5;
+var alarm = new Audio('alert.mp3');
+var loop = 0;
+var clicks = 0;
+function btnReset() {
+    $('#sessionName').text('In Session');
+    clearInterval(countInt);
+}
+function addSessionTime() {
+    sessionLength += 1;
+    $("#timer-session").text(sessionLength);
+    $("#timeLeft").text(sessionLength);
+    $('.glow').removeClass('pulse');
+    btnReset();
+}
+function decreaseSessionTime() {
+    if (sessionLength > 1){
+        sessionLength -= 1;
+    } else {
+        sessionLength = 1;
+    }
+    $("#timer-session").text(sessionLength);
+    $("#timeLeft").text(sessionLength);
+    $('.glow').removeClass('pulse');
+    btnReset();
+}
+function decreaseBreakTime() {
+    if (breakLength > 1){
+        breakLength -= 1;
+    } else {
+        breakLength = 1;
+    }
+    $("#timer-break").text(breakLength);
+    btnReset();
+}
+function addBreakTime(){
+    breakLength += 1;
+    $("#timer-break").text(breakLength);
+    btnReset();
+}
+function startTimer() {
+    if (clicks == 0) {
+        clicks += 1;
+        seconds = 0;
+        countDown(sessionLength, seconds);
+        $('.glow').addClass('pulse');
+    } else if (clicks == 1) {
+        btnReset();
+        clicks -= 1;
+        $('.glow').removeClass('pulse');
+    }
+}
+function countDown(m,s) {
+    countInt = setInterval(function(){
+        if (m == 0 && s == 0) {
+            clearInterval(countInt);
+            if (loop == 0) {
+                timeLeft = breakLength;
+                loop += 1;
+                $('#sessionName').text('On Break');
+            } else {
+                timeLeft = sessionLength;
+                loop -= 1;
+                $('#sessionName').text('In Session');
+            }
+            alarm.play();
+            countDown(timeLeft,0);
+        } else if (s != 0) {
+            if (s <= 10){
+                s -= 1;
+                timeLeft = m + ':0' + s;
+            } else {
+                s -= 1;
+                timeLeft = m + ':' + s;
+            }
+        } else if (s == 0) {
+            s = 59;
+            m -= 1;
+            timeLeft = m + ':' + s;
+        }
+        $('#timeLeft').text(timeLeft);
+    }, 1000);
+}
+function reset() {
+    sessionLength = 25;
+    breakLength = 5;
+    $('.glow').removeClass('pulse');
+    $("#timer-session").text(sessionLength);
+    $("#timeLeft").text(sessionLength);
+    $("#timer-break").text(breakLength);
+    btnReset();
+}
+$('document').ready(function(){
+    $('#pause').hide();
+    $('#settings-panel').hide();
+    $('#play').on('click', function(){
+        $(this).hide();
+        $('#pause').show();
+    })
+    $('#pause').on('click', function(){
+        $(this).hide();
+        $('#play').show();
+    })
+    $('#setting').on('click',function(){
+        $('#settings-panel').slideToggle(100);
+    })
+});
